af41e1f6e1320dc1e4f5b548428c3ac076542bbc
Dev Jethava <dev.jethava@cybercom.in>
2023-09-04 18:51:43 +0530

    * add Coffee App code

--- src/CofeeApp/App.tsx
+++ src/CofeeApp/App.tsx
@@ -5,47 +5,18 @@
  * @format
  */
 
-import { styled } from 'nativewind';
 import React from 'react';
 import {
-    SafeAreaView,
-    StatusBar,
     StyleSheet,
-    Text,
-    View,
 } from 'react-native';
-
-const StyledView = styled(View)
-const StyledText = styled(Text)
+import AppNavigation from './navigation/appNavigation';
 
 function App(): JSX.Element {
     return (
-        <SafeAreaView className="flex-1">
-            <StatusBar barStyle={'dark-content'} />
-            <StyledView className="flex-1 items-center justify-center">
-                <StyledText className="text-slate-800">Try editing me! 🎉</StyledText>
-            </StyledView>
-        </SafeAreaView>
+        <AppNavigation />
     );
 }
 
-const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-});
+const styles = StyleSheet.create({});
 
 export default App;
--- src/CofeeApp/README.md
+++ src/CofeeApp/README.md
@@ -4,3 +4,7 @@ To learn more about Used Tech, take a look at the following resources:
 
 - Develop Coffee App UI using Tailwind CSS
 - [NativeWind](https://www.nativewind.dev/quick-starts/react-native-cli) - NativeWind uses **Tailwind CSS** as scripting language to create a universal styling system.
+<p float="left">
+<img src="https://raw.githubusercontent.com/DevJethava/RNPractice/master/src/CofeeApp/asets/screenshot/1.png" width="300"/>
+<img src="https://raw.githubusercontent.com/DevJethava/RNPractice/master/src/CofeeApp/asets/screenshot/2.png" width="300"/>
+</p>
