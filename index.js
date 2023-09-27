9b52ddf059312b846f93e6344c68eb382f8a57e2
Dev Jethava <dev.jethava@cybercom.in>
2023-09-27 14:22:06 +0530

    * add Voice Assistant App

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 import {name as appName} from './app.json';
+import App from './src/AIVoiceAssistant/App';
 // import App from './src/ReduxSagaApp/App';
-import App from './src/ReduxApp/App';
+// import App from './src/ReduxApp/App';
 // import App from './src/ChatApp/App';
 // import App from './src/CofeeApp/App';
 // import App from './src/AuthApp/App';
---
+++ src/AIVoiceAssistant/assets/index.tsx
@@ -0,0 +1,10 @@
+export const Images = {
+    BOT_IMAGE: require('./images/bot.png'),
+    WELCOME_IMAGE: require('./images/welcome.png'),
+    LOADING_GIF: require('./images/loading.gif'),
+    VOICE_GIF: require('./images/voiceLoading.gif'),
+    RECORDING_IMAGE: require('./images/recordingIcon.png'),
+    CHATGPT_IMAGE: require('./images/chatgptIcon.png'),
+    DALLE_ICON_IMAGE: require('./images/dalleIcon.png'),
+    SMARTAIL_ICON_IMAGE: require('./images/smartaiIcon.png')
+}
